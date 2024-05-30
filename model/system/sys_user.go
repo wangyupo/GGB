ab445204d686712a2078cdb6e1e38ba49310a8ae
wangyupo <1106774262@qq.com>
2024-05-30 18:33:19 +0800

    [^]用户模型新增Password字段

--- model/system/sys_user.go
+++ model/system/sys_user.go
@@ -2,10 +2,12 @@ package system
 
 import "github.com/wangyupo/GGB/global"
 
+// SysUser 系统用户
 type SysUser struct {
 	global.BaseModel
-	UserName string `json:"userName" gorm:"index;comment:用户登录名"`
-	NickName string `json:"nickName" gorm:"default:系统用户;comment:用户昵称"`
-	Email    string `json:"email" gorm:"comment:用户邮箱"`
+	UserName string `json:"userName" gorm:"size:64;index;comment:用户登录名"`
+	NickName string `json:"nickName" gorm:"size:64;default:系统用户;comment:用户昵称"`
+	Email    string `json:"email" gorm:"size:128;comment:用户邮箱"`
+	Password string `json:"password" gorm:"comment:密码"`
 	Status   int    `json:"status" gorm:"default:1;comment:用户状态 1正常 2冻结"`
 }
