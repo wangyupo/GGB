d3b6a6f11b3e25975beec0f14c02dacd0eb3580d
wangyupo <1106774262@qq.com>
2024-05-31 16:11:43 +0800

    [+]入参password增加类型

--- utils/hash.go
+++ utils/hash.go
@@ -9,7 +9,7 @@ func BcryptHash(password string) string {
 }
 
 // BcryptCheck 对比明文密码和数据库的哈希值
-func BcryptCheck(password, hash string) bool {
+func BcryptCheck(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
