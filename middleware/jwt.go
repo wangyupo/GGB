3bece640b15e96557a2031bcd4ed500cf15db004
wangyupo <1106774262@qq.com>
2024-05-31 18:48:16 +0800

    [^]补充身份校验中间件

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,18 +1,39 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/wangyupo/GGB/model/common/response"
 	"github.com/wangyupo/GGB/utils"
 )
 
 func Jwt() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 从浏览器 cookie 中拿 x-token
 		token := utils.GetToken(c)
 		if token == "" {
 			response.NoAuth("未登录或非法访问", c)
 			c.Abort()
 			return
 		}
+
+		// parseToken 解析 token 包含的信息
+		claims, err := utils.ParseToken(token)
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenExpired) {
+				response.NoAuth("授权已过期", c)
+				utils.ClearToken(c)
+				c.Abort()
+				return
+			}
+			response.NoAuth(err.Error(), c)
+			utils.ClearToken(c)
+			c.Abort()
+			return
+		}
+
+		c.Set("claims", claims)
+		c.Next()
 	}
 }
