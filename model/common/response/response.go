f14fbb671f47bbb2bafa26b78ed519c87509152b
wangyupo <1106774262@qq.com>
2024-05-31 18:54:38 +0800

    [^]优化返回code

--- model/common/response/response.go
+++ model/common/response/response.go
@@ -17,8 +17,9 @@ type MsgResponse struct {
 }
 
 const (
-	ERROR   = 7
-	SUCCESS = 0
+	ErrorToken = -1
+	Error      = 7
+	Success    = 0
 )
 
 func Result(code int, data interface{}, msg string, c *gin.Context) {
@@ -36,26 +37,30 @@ func MsgResult(code int, msg string, c *gin.Context) {
 	})
 }
 
+// SuccessWithMessage 返回成功并携带自定义消息
 func SuccessWithMessage(message string, c *gin.Context) {
-	MsgResult(SUCCESS, message, c)
+	MsgResult(Success, message, c)
 }
 
+// SuccessWithData 返回成功并携带自定义数据和默认消息
 func SuccessWithData(data interface{}, c *gin.Context) {
-	Result(SUCCESS, data, "查询成功", c)
+	Result(Success, data, "查询成功", c)
 }
 
+// SuccessWithDetailed 返回成功并携带自定义数据和自定义消息
 func SuccessWithDetailed(data interface{}, message string, c *gin.Context) {
-	Result(SUCCESS, data, message, c)
+	Result(Success, data, message, c)
 }
 
+// FailWithMessage 返回失败并携带自定义消息
 func FailWithMessage(message string, c *gin.Context) {
-	MsgResult(ERROR, message, c)
+	MsgResult(Error, message, c)
 }
 
+// NoAuth 返回身份校验不通过并携带自定义消息
 func NoAuth(message string, c *gin.Context) {
-	c.JSON(http.StatusUnauthorized, Response{
-		7,
-		nil,
+	c.JSON(http.StatusUnauthorized, MsgResponse{
+		ErrorToken,
 		message,
 	})
 }
