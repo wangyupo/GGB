1ec88f159cb4862a4a93c414685c479ec64f8173
wangyupo <1106774262@qq.com>
2024-06-06 17:47:57 +0800

    [^]函数GetPaginationParams内部计算offse和limit

--- utils/pagination.go
+++ utils/pagination.go
@@ -5,7 +5,8 @@ import "github.com/gin-gonic/gin"
 // GetPaginationParams 获取分页参数
 func GetPaginationParams(c *gin.Context) (int, int) {
 	// 你可以在这里修改下参数名，例如page和limit
-	pageNumber := GetQueryInt(c, "pageNumber", 1)
-	pageSize := GetQueryInt(c, "pageSize", 10)
-	return pageNumber, pageSize
+	offset := GetQueryInt(c, "pageNumber", 1)
+	limit := GetQueryInt(c, "pageSize", 10)
+	offset = (offset - 1) * limit
+	return offset, limit
 }
