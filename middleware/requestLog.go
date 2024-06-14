11cb0852194526feb30bcc6f198dc963f89e68c3
wangyupo <1106774262@qq.com>
2024-06-14 18:35:22 +0800

    [^]记录请求日志中间件

--- middleware/requestLog.go
+++ middleware/requestLog.go
@@ -1,10 +1,27 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"github.com/wangyupo/GGB/utils"
+	"io"
+)
 
 // RequestLog 记录请求日志中间件
 func RequestLog() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 读取请求数据
+		path := c.Request.URL.Path
+		method := c.Request.Method
+		queryParams := c.Request.URL.RawQuery
+		userId, _ := utils.GetUserID(c)
 
+		bodyBytes, _ := io.ReadAll(c.Request.Body) // Body 的内容被消耗掉，读取到了内存
+
+		requestBody := string(bodyBytes)
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // 重新设置 c.Request.Body，以便请求体可以在后续的处理中再次读取
+
+		// 记录响应数据
+		responseWriter := gin.ResponseWriter()
 	}
 }
