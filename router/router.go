85138a61df45329dc23f4aee16fdf7225a132421
wangyupo <1106774262@qq.com>
2024-06-17 17:18:30 +0800

    [^]router声明语义化，ApiGroup->PublicGroup、PrivateGroup

--- router/router.go
+++ router/router.go
@@ -3,12 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	apiSystem "github.com/wangyupo/GGB/api/system"
+	"github.com/wangyupo/GGB/global"
 	"github.com/wangyupo/GGB/middleware"
 	middlewareLog "github.com/wangyupo/GGB/middleware/log"
 	"github.com/wangyupo/GGB/router/log"
 	"github.com/wangyupo/GGB/router/system"
 	"go.uber.org/zap"
-	"os"
 )
 
 func InitRouter() *gin.Engine {
@@ -16,25 +16,28 @@ func InitRouter() *gin.Engine {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	r := gin.Default()
+	Router := gin.Default()
 
-	ApiGroup := r.Group(os.Getenv("ROUTE_PREFIX"))
+	// 路由-不做鉴权
+	PublicGroup := Router.Group(global.GGB_CONFIG.System.RouterPrefix)
 	{
-		ApiGroup.POST("/login", apiSystem.Login) // 登录
+		PublicGroup.POST("/login", apiSystem.Login) // 登录
 	}
 
-	ApiGroup.Use(middleware.Logger(logger)).Use(middleware.Jwt())
+	// 路由-需要鉴权
+	PrivateGroup := Router.Group(global.GGB_CONFIG.System.RouterPrefix)
+	PrivateGroup.Use(middleware.Logger(logger)).Use(middleware.Jwt())
 	{
-		ApiGroup.POST("/logout", middlewareLog.LoginLog(2), apiSystem.Logout) // 登出
+		PrivateGroup.POST("/logout", middlewareLog.LoginLog(2), apiSystem.Logout) // 登出
 
-		system.SysUserRouter(ApiGroup)
-		system.SysRoleRouter(ApiGroup)
-		system.SysMenuRouter(ApiGroup)
-		system.SysDictCategoryRouter(ApiGroup)
-		system.SysDictDataRouter(ApiGroup)
+		system.SysUserRouter(PrivateGroup)         // 用户管理
+		system.SysRoleRouter(PrivateGroup)         // 角色管理
+		system.SysMenuRouter(PrivateGroup)         // 菜单管理
+		system.SysDictCategoryRouter(PrivateGroup) // 字典类型管理
+		system.SysDictDataRouter(PrivateGroup)     // 字典数据管理
 
-		log.LoginRouter(ApiGroup)
+		log.LoginRouter(PrivateGroup) // 登录日志
 	}
 
-	return r
+	return Router
 }
