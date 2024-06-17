44e96c246627cde626cf27f33032d8b6223813a3
wangyupo <1106774262@qq.com>
2024-06-17 17:13:58 +0800

    [+]新增全局变量GGB_CONFIG

--- global/global.go
+++ global/global.go
@@ -1,7 +1,15 @@
 package global
 
-import "gorm.io/gorm"
+import (
+	"github.com/spf13/viper"
+	"github.com/wangyupo/GGB/config"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
 
 var (
-	GGB_DB *gorm.DB
+	GGB_DB     *gorm.DB
+	GGB_CONFIG config.Server
+	GGB_VP     *viper.Viper
+	GGB_LOG    *zap.Logger
 )
