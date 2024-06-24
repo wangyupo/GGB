1f421c5aba3a61ef7dae89fb381ef9375caf5622
wangyupo <1106774262@qq.com>
2024-06-24 15:12:55 +0800

    [+]新增zap的全局配置

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 type Server struct {
+	Zap    Zap    `mapstructure:"zap" json:"zap" yaml:"zap"`
 	System System `mapstructure:"system" json:"system" yaml:"system"`
 	// gorm
 	Mysql Mysql `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
