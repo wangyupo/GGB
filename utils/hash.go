1754a8ee4e8ed3b91dd6a37603b13a88244e828c
wangyupo <1106774262@qq.com>
2024-06-27 15:44:57 +0800

    [+]新增MD5V函数

--- utils/hash.go
+++ utils/hash.go
@@ -1,6 +1,10 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // BcryptHash 使用 bcrypt 对密码进行加密
 func BcryptHash(password string) string {
@@ -12,4 +16,11 @@ func BcryptHash(password string) string {
 func BcryptCheck(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+// MD5V md5加密
+func MD5V(str []byte, b ...byte) string {
+	h := md5.New()
+	h.Write(str)
+	return hex.EncodeToString(h.Sum(b))
 }
