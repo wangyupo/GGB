eb5290523d15f5dc012377e810cd393c22bb3efa
wangyupo <1106774262@qq.com>
2024-07-01 17:55:46 +0800

    [^]优化Model，增加gorm声明类型、长度

--- model/system/sys_menu.go
+++ model/system/sys_menu.go
@@ -5,10 +5,10 @@ import "github.com/wangyupo/GGB/global"
 // SysMenu 系统菜单
 type SysMenu struct {
 	global.BaseModel
-	Label    string `json:"label" gorm:"size:128;not null;comment:菜单名称"`
-	Path     string `json:"path" gorm:"comment:菜单路径"`
-	Icon     string `json:"icon" gorm:"comment:菜单图标"`
+	Label    string `json:"label" gorm:"type:varchar(64);not null;comment:菜单名称"`
+	Path     string `json:"path" gorm:"type:varchar(128);comment:菜单路径"`
+	Icon     string `json:"icon" gorm:"type:varchar(64);comment:菜单图标"`
 	ParentId uint   `json:"parentId" gorm:"default:0;comment:父级菜单ID"`
-	Sort     int    `json:"sort" gorm:"default:0;comment:排序号（越小越靠前）"`
-	Type     uint   `json:"type" gorm:"default:1;comment:菜单类型 1菜单 2页面"`
+	Sort     int    `json:"sort" gorm:"type:int(3);default:0;comment:排序号（越小越靠前）"`
+	Type     uint   `json:"type" gorm:"type:tinyint(1);default:1;comment:菜单类型 1菜单 2页面"`
 }
--- model/system/sys_role.go
+++ model/system/sys_role.go
@@ -5,8 +5,8 @@ import "github.com/wangyupo/GGB/global"
 // SysRole 系统角色
 type SysRole struct {
 	global.BaseModel
-	RoleName    string `json:"roleName" form:"roleName" gorm:"unique;index;size:128;comment:角色名，用于显示"`
-	RoleCode    string `json:"roleCode" gorm:"unique;comment:角色的唯一标识码"`
-	Description string `json:"description" gorm:"comment:角色描述等信息"`
-	Status      uint   `json:"status" gorm:"default:0;comment:角色状态 0禁用 1启用"`
+	RoleName    string `json:"roleName" form:"roleName" gorm:"type:varchar(64);unique;index;size:128;comment:角色名，用于显示"`
+	RoleCode    string `json:"roleCode" gorm:"type:varchar(64);unique;comment:角色的唯一标识码"`
+	Description string `json:"description" gorm:"type:varchar(128);comment:角色描述等信息"`
+	Status      uint   `json:"status" gorm:"type:tinyint(1);default:0;comment:角色状态 0禁用 1启用"`
 }
