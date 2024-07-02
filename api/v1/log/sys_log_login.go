92375ed1a1ccfbe5be300d2b4f23813790ff80bb
wangyupo <1106774262@qq.com>
2024-07-02 14:39:04 +0800

    [^]优化数据组装方式

--- api/v1/log/sys_log_login.go
+++ api/v1/log/sys_log_login.go
@@ -4,9 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/wangyupo/GGB/global"
 	"github.com/wangyupo/GGB/model/common/response"
+	"github.com/wangyupo/GGB/model/log"
 	"github.com/wangyupo/GGB/utils"
 	"go.uber.org/zap"
-	"reflect"
 	"time"
 )
 
@@ -37,7 +37,7 @@ func (s *SysLogLoginApi) GetSysLogLoginList(c *gin.Context) {
 func (s *SysLogLoginApi) ExportExcel(c *gin.Context) {
 	userId, _ := utils.Str2uint(c.Query("userId"))
 
-	list, _, err := sysLoginLogService.GetSysLogLoginList(userId, 1, 999)
+	list, _, err := sysLoginLogService.GetSysLogLoginList(userId, 0, 999)
 	if err != nil {
 		global.GGB_LOG.Error("获取登录日志列表失败！", zap.Error(err))
 		response.FailWithMessage(err.Error(), c)
@@ -46,21 +46,13 @@ func (s *SysLogLoginApi) ExportExcel(c *gin.Context) {
 
 	var ExcelList [][]interface{}
 	ExcelList = append(ExcelList, []interface{}{"用户名称", "ip地址", "操作类型", "记录时间"})
-
-	ColumnKey := []string{"userName", "ip", "type", "createdAt"}
-	for _, listItem := range list.([]map[string]interface{}) {
+	for _, row := range list.([]log.SysLogLogin) {
 		var listValueItem []interface{}
-		for _, key := range ColumnKey {
-			value := listItem[key]
-			if reflect.TypeOf(listItem[key]) == reflect.TypeOf(time.Time{}) {
-				value = value.(time.Time).Format(time.DateTime)
-			}
-			listValueItem = append(listValueItem, value)
-		}
+		listValueItem = append(listValueItem, row.UserName, row.IP, row.TypeText, row.CreatedAt.Format(time.DateTime))
 		ExcelList = append(ExcelList, listValueItem)
 	}
 
-	filePath, _ := utils.ExportExcelByTemplate(ExcelList)
+	filePath, _ := utils.CreateExcelByList(ExcelList)
 
 	// 使用Gin的c.File方法直接提供该文件进行下载
 	c.File(filePath)
