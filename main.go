5751e0c9197e05fe3913f12449b1f62571b4c712
wangyupo <1106774262@qq.com>
2024-07-05 16:44:49 +0800

    [^]优化注释&&新增初始化校验器

--- main.go
+++ main.go
@@ -16,13 +16,14 @@ func main() {
 	global.GGB_VP = core.Viper()       // 初始化Viper
 	global.GGB_LOG = core.Zap()        // 初始化日志库
 	zap.ReplaceGlobals(global.GGB_LOG) // 替换全局的日志记录器，可以在程序的任何地方通过 zap.L() 函数来获取这个全局日志记录器，并进行日志记录
-	global.GGB_DB = initialize.Gorm()  // gorm连接数据库
-	initialize.Timer()
+	global.GGB_DB = initialize.Gorm()  // 初始化gorm，连接数据库
+	initialize.Timer()                 // 初始化定时任务
+	initialize.Validator("zh")         // 初始化校验器
 	if global.GGB_DB != nil {
 		initialize.RegisterTables() // 初始化表
-		// 程序结束前关闭数据库链接
-		db, _ := global.GGB_DB.DB()
+		db, _ := global.GGB_DB.DB() // 程序结束前关闭数据库链接
 		defer db.Close()
 	}
+
 	core.RunWindowsServer()
 }
