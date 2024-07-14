2d7f69dc72eb2f43df714ac71c5ebc0af9e19a56
wangyupo <1106774262@qq.com>
2024-07-14 17:08:40 +0800

    [^]增加服务启动后的欢迎语

--- core/server.go
+++ core/server.go
@@ -26,6 +26,12 @@ func RunWindowsServer() {
 		MaxHeaderBytes: 1 << 20,          // 请求头的最大字节数，1 MB
 	}
 
+	fmt.Printf(`
+ggb_server 服务启动成功
+项目地址：		https://github.com/wangyupo/GGB
+OpenAPI 文档地址：http://127.0.0.1%s/swagger/index.html
+	`, address)
+
 	// 使用http.Server 监听和服务
 	err := server.ListenAndServe()
 	if err != nil {
