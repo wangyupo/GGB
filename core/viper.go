495e539c51321b68c2d4ca8d007e7ef6f5651411
wangyupo <1106774262@qq.com>
2024-07-14 22:02:32 +0800

    [^]优化提示文案

--- core/viper.go
+++ core/viper.go
@@ -23,24 +23,24 @@ func Viper(path ...string) *viper.Viper {
 				switch gin.Mode() {
 				case gin.DebugMode:
 					config = internal.ConfigDefaultFile
-					fmt.Printf("您正在使用gin模式的%s环境名称，config的路径为%s\n", gin.Mode(), internal.ConfigDefaultFile)
+					fmt.Printf("您正在使用 gin 模式的 %s 环境名称，config 的路径为 %s\n", gin.Mode(), internal.ConfigDefaultFile)
 				case gin.ReleaseMode:
 					config = internal.ConfigReleaseFile
-					fmt.Printf("您正在使用gin模式的%s环境名称，config的路径为%s\n", gin.Mode(), internal.ConfigDefaultFile)
+					fmt.Printf("您正在使用 gin 模式的 %s 环境名称，config 的路径为 %s\n", gin.Mode(), internal.ConfigDefaultFile)
 				case gin.TestMode:
 					config = internal.ConfigTestFile
-					fmt.Printf("您正在使用gin模式的%s环境名称，config的路径为%s\n", gin.Mode(), internal.ConfigDefaultFile)
+					fmt.Printf("您正在使用 gin 模式的 %s 环境名称，config 的路径为 %s\n", gin.Mode(), internal.ConfigDefaultFile)
 				}
 			} else {
 				config = configEnv
-				fmt.Printf("您正在使用%s环境变量，config的路径为%s\n", internal.ConfigEnv, config)
+				fmt.Printf("您正在使用 %s 环境变量，config 的路径为 %s\n", internal.ConfigEnv, config)
 			}
 		} else {
-			fmt.Printf("您在在使用命令行-c参数传递的值，config的路径为%s\n", config)
+			fmt.Printf("您正在使用命令行 -c 参数传递的值，config 的路径为 %s\n", config)
 		}
 	} else {
 		config = path[0]
-		fmt.Printf("您正在使用func Viper()传递的值，config的路径为%s\n", config)
+		fmt.Printf("您正在使用 func Viper() 传递的值，config 的路径为 %s\n", config)
 	}
 
 	v := viper.New()
